39c1a971bdddc10fe22abd43e40e7b8dd306cb1b
Ayoubmansour8 <ayoub.mansour8@gmail.com>
2023-08-01 14:06:00 -0700

    all

--- alx-low_level_programming/0x13-more_singly_linked_lists/0-print_listint.c
+++ alx-low_level_programming/0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t num = 0; /* Initialize a variable to count the number of nodes */
 
 	while (h)
 	{
-		printf("%d\n", h->n);
-		num++;
-		h = h->next;
+		printf("%d\n", h->n); /* Print the integer value 'n' of the current node */
+		num++; /* Increment the node count */
+		h = h->next; /* Move to the next node */
 	}
 
-	return (num);
+	return (num); /* Return the total number of nodes printed */
 }
 
